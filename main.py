120e877fc71639bbd59200f2dd6d01a3fe8856d9
jpetrasek1217 <josephpetrasek1017@gmail.com>
2026-01-05 11:18:29 -0500

    Adding allowed origin

--- main.py
+++ main.py
@@ -32,7 +32,10 @@ app = FastAPI(title="YouTube CTR Predictor", lifespan=lifespan)
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://jpetrasek1217.github.io"  # Add your GitHub Pages URL
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
